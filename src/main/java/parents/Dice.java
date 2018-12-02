5069da3946c6951d1bcbf71e1a6ab6395128b0aa
Nyalls Hemingway <hemingwaynyalls@gmail.com>
2018-12-02 11:46:16 +0000

    added dice functionality - still to implement

---
+++ src/main/java/parents/Dice.java
@@ -0,0 +1,29 @@
+package parents;
+
+public class Dice {
+
+
+    public Dice() {
+
+    }
+
+    public int rollD4(){
+        return 1;
+    }
+
+    public int rollD6(){
+        return 1;
+    }
+
+    public int rollD8(){
+        return 1;
+    }
+
+    public int rollD10(){
+        return 1;
+    }
+
+    public int rollD12(){
+        return 1;
+    }
+}
